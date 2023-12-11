99211bf2f898abcc37e7206e31c32f14d9caf606
Ricardo Mena <rmenadclases@gmail.com>
2023-12-11 10:59:29 +0100

    Elimianmos los archivos ya que al hacer el reverse estamos teniendo problemas

--- validadores/cerrarSesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    session_start(); // este por si no la has iniciado en la pagina que planeas destruirla, de lo contrario no te destruirá nada
-    session_destroy();
-    session_unset();
-    header("location: ../plantilla/Login.html"); // Redirige al usuario
-
-?>
--- validadores/comprobarPerfil.php
+++
@@ -1,9 +0,0 @@
-<?php
- if (!isset($_REQUEST['btnAceptar'])) {   
-    echo '<div class="centrar">';
-    echo '<label id="labelCookies" for="btnAceptar" class="color">Haz clic para aceptar las cookies:</label>';
-    echo '<input id="btnAceptar" name="btnAceptar" type="button" value="Aceptar" onclick="ocultarBoton();">';                               
-} else {
-    setcookie('cookiesAceptadas', 'cookiesAceptadas');
-}
-?>
